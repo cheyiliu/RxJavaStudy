1785d54be3cc50aa911c95284611256b79fe3515
hsy <houshengyong@diditaxi.com.cn>
2016-01-22 12:44:29 +0800

    add module rxjava rxandroid

--- Demo/app/src/main/java/com/study/rx/demo/MainActivity.java
+++ Demo/app/src/main/java/com/study/rx/demo/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import rx.functions.Action0;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        // simple usage of schedule
+        Log.i("test", "now thread, " + Thread.currentThread().getId());
+        Schedulers.io().createWorker().schedule(new Action0() {
+            @Override
+            public void call() {
+                Log.i("test", "hi from thread, " + Thread.currentThread().getId());
             }
         });
     }
